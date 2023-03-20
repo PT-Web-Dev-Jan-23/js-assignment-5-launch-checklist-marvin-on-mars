f16b30a8acedb8f41fc4ad872ee2eaf08fab9bdc
marvin-on-mars <marvin.jones95@yahoo.com>
2023-03-20 06:45:36 -0500

    Alerts added and updated

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
+    let pilot = document.querySelector("input[name=pilotName]").value;
+    let copilot = document.querySelector("input[name=copilotName]").value;
+    let fuelLevel = document.querySelector("input [name=fuelLevel]").value;
+    let cargoLevel = document.querySelector("input[name=cargoLevel]").value;
+    let list = document.getElementById('faultyItems');
 
-   let listedPlanets;
+    formSubmission(document,list,pilot,copilot,fuelLevel,cargoLevel);
+})
+
+
+   let listedPlanets = myFetch();
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse;
    listedPlanetsResponse.then(function (result) {
@@ -11,6 +20,14 @@ window.addEventListener("load", function() {
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+       let planet = pickPlanet(listedPlanets);
+       let name = planet.name;
+       let diameter = planet.diameter;
+       let star = planet.star;
+       let distance = planet.distance;
+       let imageUrl = planet.image;
+       let moons = planet.moons;
+       addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl);
    })
    
 });
